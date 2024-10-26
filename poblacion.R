ff5e4051826a480daa1e7e1d962be2ada267402c
Iván Paredes Reséndiz <ivanresendiz25@gmail.com>
2024-10-25 19:47:19 -0600

    Update poblacion.R

--- poblacion.R
+++ poblacion.R
@@ -1,9 +1,13 @@
 library(jsonlite)
 library(rjson)
 
+
+# URL del API
 url <- "https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/00000/false/BISE/2.0/af2d1a87-37aa-4c76-0d9e-56471598b0f7?type=json"
-# Llamada al API y carga del JSON
-respuesta <- fromJSON(url)
+
+# Llamada al API usando curl
+json_data <- readLines(url(url, "r"), warn = FALSE)
+respuesta <- fromJSON(paste(json_data, collapse = ""))
 
 datosGenerales<-content(respuesta,"text")
 flujoDatos<-paste(datosGenerales,collapse = " ")
